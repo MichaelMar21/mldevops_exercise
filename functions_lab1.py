6c254c9d1a03ebed1e161c028e6763b17dd5938d
Michal Marusinski <mmarusinski123@wp.pl>
2024-11-18 18:58:53 +0100

    Adding Python files

---
+++ functions_lab1.py
@@ -0,0 +1,41 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# Funkcja treningowa
+def train(model, device, train_loader, optimizer, epoch):
+
+    model.train()
+    criterion = nn.CrossEntropyLoss()
+    
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(f'Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} '
+                  f'({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
+
+# Funkcja testowa
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    criterion = nn.CrossEntropyLoss()
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+    print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} '
+          f'({accuracy:.0f}%)\n')
